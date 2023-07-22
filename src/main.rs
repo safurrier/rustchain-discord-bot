31d12d287a3f7ad6ab3e1cd4e6749047cfbfaab5
alex furrier <alex.furrier@discordapp.com>
2023-07-22 16:49:41 -0700

    Basic OAI slash command

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use dotenv::dotenv;
-use poise::{framework, serenity_prelude as serenity};
+use llm_chain::{executor, parameters, prompt};
+use poise::serenity_prelude as serenity;
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[poise::command(prefix_command, slash_command)]
-async fn age(
+async fn greeting(
     ctx: Context<'_>,
     #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
-    let u: &serenity::User = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response: String = format!("{} account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
+    let user: &serenity::User = user.as_ref().unwrap_or_else(|| ctx.author());
+
+    // Create a new ChatGPT executor
+    let exec = executor!()?;
+
+    let greeting_prompt = format!(
+        "You are a robot assistant for making personalized greetings. \
+        Make a personalized greeting for {}",
+        user.name
+    );
+
+    // Create our prompt...
+    let res = prompt!(greeting_prompt)
+        .run(&parameters!(), &exec) // ...and run it
+        .await?;
+    ctx.say(res.to_string()).await?;
     Ok(())
 }
 
@@ -20,14 +34,10 @@ async fn main() {
     dotenv().ok();
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![greeting()],
             ..Default::default()
         })
         .intents(serenity::GatewayIntents::MESSAGE_CONTENT)
-        // .intents(serenity::GatewayIntents::all())
-        // .intents(
-        //     serenity::GatewayIntents::GUILD_MESSAGES | serenity::GatewayIntents::DIRECT_MESSAGES,
-        // )
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
